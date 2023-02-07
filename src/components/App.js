5984e6297b500376bb362cc862ffd5fa53973f0f
triciahughes <hughes.a.tricia@gmail.com>
2023-02-07 08:36:55 -0800

    first deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then((data) => {
+        setListings(data);
+      });
+  }, []);
+
+  //console.log(listings);
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,22 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings }) {
+  const listingsCard = listings.map((listing) => {
+    return (
+      <ListingCard
+        key={listing.id}
+        description={listing.description}
+        image={listing.image}
+        location={listing.location}
+      />
+    );
+  });
   return (
     <main>
       <ul className="cards">
         {/* use the ListingCard component to display listings */}
+        {listingsCard}
       </ul>
     </main>
   );
